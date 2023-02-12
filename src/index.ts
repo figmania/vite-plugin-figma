0766fa6c70a0a36cf7fbb49f695a1d0c12fdc298
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-02-12 17:54:45 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { build } from 'esbuild'
+import { join } from 'path'
+import { Plugin } from 'vite'
+
+export interface FigmaOptions {
+  name: string
+  id: string
+  editorType: ('figma' | 'figjam')[]
+  api: string
+  main: string
+}
+
+const config = { root: '', outDir: '' }
+
+export function figma({ main, api, id, name, editorType }: FigmaOptions): Plugin {
+  return {
+    name: 'vite-plugin-figma',
+    apply: 'build',
+    configResolved(opts) {
+      config.root = opts.root
+      config.outDir = opts.build.outDir
+    },
+    generateBundle: {
+      order: 'post',
+      async handler() {
+        // Generate Main
+        const entry = join(config.root, main)
+        const result = await build({ entryPoints: [entry], target: 'ES2015', bundle: true, write: false }).catch(() => process.exit(1))
+        this.emitFile({ type: 'asset', fileName: 'main.js', name: 'main', source: result.outputFiles[0].text })
+
+        // Generate Manifest
+        const source = JSON.stringify({
+          name,
+          id,
+          editorType,
+          api,
+          ui: 'index.html',
+          main: 'main.js',
+        }, null, 2)
+        this.emitFile({ type: 'asset', fileName: 'manifest.json', name: 'manifest', source })
+      }
+    }
+  }
+}
