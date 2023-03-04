2bd76a7a887f1a1bbedd3ae57aee9fa2a605839f
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-04 13:22:34 +0800

    refactor vite plugin & bundle single file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { build, BuildOptions } from 'esbuild'
+import { build } from 'esbuild'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
-import { Plugin } from 'vite'
+import { Plugin, ResolvedConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export interface FigmaOptions {
   name: string
@@ -8,38 +10,69 @@ export interface FigmaOptions {
   editorType: ('figma' | 'figjam')[]
   api: string
   main: string
-  esbuild?: BuildOptions
 }
 
-const config = { root: '', outDir: '' }
+const buildDevHtml = (html: string, config: ResolvedConfig) => html.replace('</head>', `
+    <base href="http://${config.server.host}:${config.server.port}/">
+    <script type="module">
+      import RefreshRuntime from "/@react-refresh"
+      RefreshRuntime.injectIntoGlobalHook(window)
+      window.$RefreshReg$ = () => {}
+      window.$RefreshSig$ = () => (type) => type
+      window.__vite_plugin_react_preamble_installed__ = true
+    </script>
+    <script type="module" src="/@vite/client"></script>
+  </head>`)
 
-export function figma({ main, api, id, name, editorType, esbuild }: FigmaOptions): Plugin {
-  return {
+async function buildMain(options: FigmaOptions, config: ResolvedConfig): Promise<string> {
+  const entry = join(config.root, options.main)
+  const result = await build({
+    entryPoints: [entry],
+    target: 'ES2015',
+    bundle: true,
+    write: false,
+    sourcemap: config.mode === 'production' ? false : 'inline',
+    minify: config.mode === 'production',
+    legalComments: 'none'
+  }).catch(() => process.exit(1))
+  return result.outputFiles[0].text
+}
+
+function buildManifest({ name, id, editorType, api }: FigmaOptions): string {
+  return JSON.stringify({ name, id, editorType, api, ui: 'index.html', main: 'main.js' }, null, 2)
+}
+
+export function figma(command: 'build' | 'serve', options: FigmaOptions): Plugin[] {
+  const config = {} as ResolvedConfig
+  return [{
     name: 'vite-plugin-figma',
-    apply: 'build',
-    configResolved(opts) {
-      config.root = opts.root
-      config.outDir = opts.build.outDir
+    config(cfg, env) {
+      return {
+        build: {
+          minify: env.mode !== 'production',
+          sourcemap: env.mode === 'production' ? false : 'inline'
+        },
+        esbuild: {
+          legalComments: 'none'
+        }
+      }
+    },
+    async configResolved(userConfig) {
+      Object.assign(config, userConfig)
+      if (config.command !== 'serve') { return }
+      if (!config.server.host || !config.server.port) { throw new Error('Server Host and Port required') }
+      if (!existsSync(config.build.outDir)) { mkdirSync(config.build.outDir) }
+      const html = readFileSync(join(config.root, 'index.html'), 'utf8')
+      writeFileSync(join(config.build.outDir, 'index.html'), buildDevHtml(html, config), 'utf-8')
+      writeFileSync(join(config.build.outDir, 'main.js'), await buildMain(options, config), 'utf-8')
+      writeFileSync(join(config.build.outDir, 'manifest.json'), buildManifest(options), 'utf-8')
     },
     generateBundle: {
       order: 'post',
       async handler() {
-        // Generate Main
-        const entry = join(config.root, main)
-        const result = await build({ ...esbuild, entryPoints: [entry], target: 'ES2015', bundle: true, write: false, sourcemap: 'inline' }).catch(() => process.exit(1))
-        this.emitFile({ type: 'asset', fileName: 'main.js', name: 'main', source: result.outputFiles[0].text })
-
-        // Generate Manifest
-        const source = JSON.stringify({
-          name,
-          id,
-          editorType,
-          api,
-          ui: 'index.html',
-          main: 'main.js',
-        }, null, 2)
-        this.emitFile({ type: 'asset', fileName: 'manifest.json', name: 'manifest', source })
+        this.emitFile({ type: 'asset', fileName: 'main.js', name: 'main', source: await buildMain(options, config) })
+        this.emitFile({ type: 'asset', fileName: 'manifest.json', name: 'manifest', source: buildManifest(options) })
       }
     }
-  }
+  }, ...(command === 'build') ? [viteSingleFile()] : []]
 }
